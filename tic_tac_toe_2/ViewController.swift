6935e66e326071f370f90f1df1eaa7c36e211f10
COE-02 <coe-02@coe-02s-imac.local>
2019-09-26 13:33:45 +0530

    Initial Commit

---
+++ tic_tac_toe_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tic_tac_toe_2
+//
+//  Created by COE-02 on 26/09/19.
+//  Copyright © 2019 COE-02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
